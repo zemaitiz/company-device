a2cc9acdacdfca7ec6638c90e539372d945c93a2
linadav <lina_dav@yahoo.co.uk>
2019-11-24 11:35:15 +0200

    Production Type

--- src/companydevice/ProductionType.java
+++ src/companydevice/ProductionType.java
@@ -1,4 +1,20 @@
 package companydevice;
 
-public class ProductionType {
+public enum ProductionType {
+    //TECHNOLOGIES, AUTOMOBILES, GARDEN_TOOLS, FOOD
+    TECHNOLOGIES("technologijos"),
+    AUTOMOBILES("autombiliai"),
+    GARDEN_TOOLS("sodas&darzas"),
+    FOOD("maistas");
+
+    private String productionDescription;
+
+    ProductionType(String productionDescription) {
+        this.productionDescription = productionDescription;
+    }
+
+    public String getProductionDescription() {
+        return productionDescription;
+    }
 }
+
